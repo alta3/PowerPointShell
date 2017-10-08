bf862a01fe85045db55636502aafe20962067f87
Michael Smythe <michael.smythe@outlook.com>
2017-10-08 18:33:43 -0400

    Improved subcommand interface

--- pps.ps1
+++ pps.ps1
@@ -5,31 +5,20 @@ param(
     $cmd,
     [switch]$help
 )
-# Set default global variables
-$cmds = New-Object System.Collections.ArrayList
-$cmd_found = $false
 
-# Loop through all available commands
-foreach($_ in Get-ChildItem $PSScriptRoot\src\cmd -Name) {
-   $cmd_name = [System.IO.Path]::GetFileNameWithoutExtension($_)
-   $cmd_help = Get-Help "$PSScriptRoot\src\cmd\$cmd_name.ps1" -Full
-   $cmds.Add($cmd_help.replace(".ps1","")) > $null
-   if ($cmd -eq $cmd_name) {
-       $cmd_found = $true
-   }
-}
-
-if ($cmd_found -eq $true) {
-    if ($help){
-	write-host Retriving help!
-        Get-Help "$PSScriptRoot\src\cmd\$cmd.ps1" -Full
+# Test to see if supplied function exists
+if (Test-Path "$PSScriptRoot\src\cmd\$cmd.ps1") { 
+    if ($help) {
+        Get-Help "$PSScriptRoot\src\cmd\$cmd.ps1"	    
     } else {
-        & "$PSScriptRoot\src\cmd\$cmd.ps1" @args
+        & "$PSScriptRoot\src\cmd\$cmd.ps1" @args 
     }
 } else {
     Write-Host "Command not recognized!"
     Write-Host "The following is a list of available commands:"
-    foreach ($_ in $cmds) {
-        Write-Host -NoNewline $_
+    foreach($_ in Get-ChildItem $PSScriptRoot\src\cmd -Name) {
+        $cmd_help = Get-Help "$PSScriptRoot\src\cmd\$_" -Full
+        $cmd_print = $cmd_help.replace(".ps1","")
+	write-host -NoNewLine $cmd_print
     }
 }
