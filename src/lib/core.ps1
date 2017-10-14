ddc7e9febe16eadfba2d784ed2110aac7c1b45cf
Michael Smythe <michael.smythe@outlook.com>
2017-10-14 15:56:55 -0400

    Updated Core Functions!

--- src/lib/core.ps1
+++ src/lib/core.ps1
@@ -2,23 +2,50 @@
 
 
 # PowerPoint Commands
-function Add-Presentation {
-    $powerpoint = New-Object -ComObject powerpoint.application 
-    $ppt = $powerpoint.presentations.add(0)
-    $ppt	
-}
 function Close-PowerPoint {
 
 }
 
-# Presentatoin Commands
-function Close-Presentation {}
-function Insert-Slide {
+function Start-PowerPoint {
+    $powerpoint = New-Object -ComObject powerpoint.application
+    $powerpoint	
+}
+
+# Presentation Commands
+function Open-Presentation {
+    [CmdletBinding()]    
     param(
-        [Parameter(Mandatory=$true)]
+        [Parameter(Position=0,Mandatory=$true)]
+        [object] $powerpoint
+	[Parameter(Position=1,Mandatory=$true)]
+	[string] $path
+	[Parameter(Position=2,Mandatory=$true)]
+	[int] $visable
+    ) 
+    $ppt = $powerpoint.open($path,,,$visable)
+    $ppt
+}
+
+function Insert-Slide {
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=$true)]
 	[object]$ppt
+	[Parameter(Position=1,Mandatory=$true)]
+	[ValidateSet("title","split","blank")]
+	[object]$type
+	[Parameter(Position=2,Mandatory=$true)]
 	[int]$index
     )
+    if ($type -eq "title") {
+        $ppt.slides.addslide($index, ppLayoutTitle)
+    } elseif ($type -eq "split") {
+        $ppt.slides.addslide($index, ppLayoutTwoContent)
+    } elseif ($type -eq "blank") {
+        $ppt.slides.addslide($index, ppLayoutBlank)
+    } else {
+        write-host "Something went very wrong!"
+    }
 }
 function Save-Presentation {}
 
