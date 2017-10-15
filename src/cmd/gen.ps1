3820b12db3b0814e397e59e26b61353561c22517
Michael Smythe <michael.smythe@outlook.com>
2017-10-15 01:35:01 -0400

    Updated generate command function

--- src/cmd/gen.ps1
+++ src/cmd/gen.ps1
@@ -1,8 +1,18 @@
+$ModulesDirectory = [Environment]::GetEnvironmentVariable("PSModulePath")
+$ModulePathOne = $PSScriptRoot "../lib/yaml/powershell-yaml.psm1"
+$ModulePathTwo = $PSScriptRoot "../lib/yaml/powershell-yaml.psd1"
+Import-Module -name $ModulePathOne
+Import-Module -name $ModulePathTwo
+
 [CmdletBinding()]
 param(
-    [string]$inFile
+    [string]$filepath
 )
 
+$yamlfile = Get-Content $filepath
+$yamlobj = ConvertFrom-Yaml $yamlfile
+
 # Create a PowerPoint presentation and keep it invisible until automation is complete
-$ppt = Add-Presentation
+$powerpoint = Add-PowerPoint
+$ppt = Add-Presentation $powerpoint 0
 
